ad89c2539b7121d55b1f3a473912ffc8c7a16777
Erick Ortega <shut435@yahoo.com>
2024-07-04 22:47:20 -0400

    added day 1 for learning python

---
+++ week1/day1/band-name-generator.py
@@ -0,0 +1,11 @@
+#1. Create a greeting for your program.
+print("Hello There")
+#2. Ask the user for the city that they grew up in.
+print("What city did you grow up in?")
+city = input()
+#3. Ask the user for the name of a pet.
+print("What is the name of your pet?")
+pet_name = input()
+#4. Combine the name of their city and pet and show them their band name.
+print("The name of your band is " + city + " " + pet_name)
+#5. Make sure the input cursor shows on a new line:
---
+++ week1/day1/day1.py
@@ -0,0 +1,63 @@
+print("Hello World")
+
+# Day 1 Print exercise using auditorium.ai (Udemy course practice)
+
+# Task 1 - print out exactly each line 
+print("1. Mix 500g of Flour, 10g Yeast and 300ml Water in a bowl.")
+print("2. Knead the dough for 10 minutes.")
+print("3. Add 3g of Salt.")
+print("4. Leave to rise for 2 hours.")
+print("5. Bake at 200 degrees C for 30 minutes.")
+
+# Task 2 - learning the difference how to add quotes inside a print statement
+print("A 'single quote' inside a double quote")
+print('A "double quote" inside a single quote')
+print("Alternatively you can just \"escape\" the quote")
+
+# Task 3 - Learning indentation
+print ("Hello" + " " + "World")
+
+
+
+# ============= Debugging Task ================
+
+# Fix the code below 👇
+
+# print(Day 1 - String Manipulation")
+# print("String Concatenation is done with the "+" sign.")
+#   print('e.g. print("Hello " + "world")')
+# print(("New lines can be created with a backslash and n.")
+
+
+# ============== Answer ================
+print("Day 1 - String Manipulation")
+print('String Concatenation is done with the "+" sign.')
+print('e.g. print("Hello " + "world")')
+print("New lines can be created with a backslash and n.")
+
+
+# Input Task 
+
+print("Hello " + input("What is your name? "))
+
+# using len to find number of characters 
+numOfLetters = len("Angela")
+print(numOfLetters)
+
+# ================= Learning Variables =====================
+
+# There are two variables, a and b from input
+a = input()
+b = input()
+# 🚨 Don't change the code above ☝️
+####################################
+# Write your code below this line 👇
+x = a
+a = b
+b = x
+
+
+# 🚨 Don't change the code below 👇
+print("a: " + a)
+print("b: " + b)
+
